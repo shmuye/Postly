39aceaa8aafa14714e8a798b0ba195aa2ca40081
Shumye Ayalneh <shmuye27@gmail.com>
2026-03-16 23:34:25 +0300

    fix redirect url

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -46,7 +46,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: window.location.origin
+      redirectTo: `${window.location.origin}/auth/callback`
     }
   });
 };
